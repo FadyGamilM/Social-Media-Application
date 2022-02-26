d63087e4f615d9caa08e308f35d765e72f126dbd
fady <gamilfady605@gmail.com>
2022-02-26 07:59:29 +0200

    Authority Protection is added to DELETE and GET Profile Functionality

--- controllers/users.js
+++ controllers/users.js
@@ -23,59 +23,26 @@ exports.VisitProfile = async (req, res, next) => {
 			message: error.message,
 		});
 	}
+};
 
-	// exports.VisitUserProfile = async (req, res, next) => {
-	// 	try {
-	// 		const user = await User.findById(req.params.id);
-	// 		if (!user) {
-	// 			return res.status(400).json({
-	// 				error: "user is not found",
-	// 			});
-	// 		}
-	// 		return res.status(200).json({
-	// 			email: user.email,
-	// 			username: user.username,
-	// 		});
-	// 	} catch (error) {
-	// 		return res.status(500).json({
-	// 			error: "server error, we are working on it",
-	// 			message: error.message,
-	// 		});
-	// 	}
-	// };
-
-	// try {
-	// 	if (req.body.userID === req.params.id || req.user.isAdmin) {
-	// 		// if user is trying to update his profile
-	// 		if (req.body.password) {
-	// 			try {
-	// 				const salt = await bcrypt.genSalt(10);
-	// 				req.body.password = await bcrypt.hash(req.body.password, salt);
-	// 			} catch (error) {
-	// 				return res.status(500).json({
-	// 					error: "server error, we are working on it",
-	// 					message: "error while updating a password" + error.message,
-	// 				});
-	// 			}
-	// 		}
-	// 		// update all other fields
-	// 		const user = await User.findByIdAndUpdate(req.params.id, {
-	// 			$set: req.body,
-	// 		});
-	// 		return res.status(200).json({
-	// 			data: "updated successfully",
-	// 		});
-	// 	} else {
-	// 		return res.status(403).json({
-	// 			error: "Not Authoraized",
-	// 		});
-	// 	}
-	// } catch (error) {
-	// 	return res.status(500).json({
-	// 		error: "server error, we are working on it",
-	// 		message: error.message,
-	// 	});
-	// }
+// @Description => User can view other users profiles
+exports.VisitUserProfile = async (req, res, next) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			return res.status(400).json({
+				error: "User is not found",
+			});
+		}
+		return res.status(200).json({
+			data: user,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			error: "server error, we are working on it",
+			message: "error.message",
+		});
+	}
 };
 
 // @Description => User can update his/her own profile only
@@ -110,12 +77,22 @@ exports.UpdateProfile = async (req, res, next) => {
 	}
 };
 
-// exports.DeleteProfile = async (req, res, next) => {
-// 	try {
-// 	} catch (error) {
-// 		return res.status(500).json({
-// 			error: "server error, we are working on it",
-// 			message: error.message,
-// 		});
-// 	}
-// };
+// @Description => User can delete his own profile only
+exports.DeleteProfile = async (req, res, next) => {
+	try {
+		if (req.params.id !== req.user.id) {
+			return res.status(500).json({
+				error: "Not Authoraized",
+			});
+		}
+		const deletedUser = await User.findByIdAndDelete(req.user.id, req.body);
+		return res.status(200).json({
+			data: deletedUser,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			error: "server error, we are working on it",
+			message: error.message,
+		});
+	}
+};
