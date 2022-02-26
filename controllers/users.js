bb4c81d9576898e7641991f30e5e189256e05216
fady <gamilfady605@gmail.com>
2022-02-26 08:26:00 +0200

    Follow Feature is added with authority

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const { User } = require("../models/User");
 const bcrypt = require("bcrypt");
 
-// @Description => logged-in user can view his/her own profile
+// @Description			logged-in user can view his/her own profile
 exports.VisitProfile = async (req, res, next) => {
 	try {
 		// req.user contains the current logged-in user
@@ -25,7 +25,7 @@ exports.VisitProfile = async (req, res, next) => {
 	}
 };
 
-// @Description => User can view other users profiles
+// @Description			User can view other users profiles
 exports.VisitUserProfile = async (req, res, next) => {
 	try {
 		const user = await User.findById(req.params.id);
@@ -45,7 +45,7 @@ exports.VisitUserProfile = async (req, res, next) => {
 	}
 };
 
-// @Description => User can update his/her own profile only
+// @Description			User can update his/her own profile only
 exports.UpdateProfile = async (req, res, next) => {
 	try {
 		if (req.params.id !== req.user.id) {
@@ -77,7 +77,7 @@ exports.UpdateProfile = async (req, res, next) => {
 	}
 };
 
-// @Description => User can delete his own profile only
+// @Description			User can delete his own profile only
 exports.DeleteProfile = async (req, res, next) => {
 	try {
 		if (req.params.id !== req.user.id) {
@@ -88,6 +88,44 @@ exports.DeleteProfile = async (req, res, next) => {
 		const deletedUser = await User.findByIdAndDelete(req.user.id, req.body);
 		return res.status(200).json({
 			data: deletedUser,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			error: "server error, we are working on it",
+			message: error.message,
+		});
+	}
+};
+
+// @Description			User can follow another user
+exports.Follow = async (req, res, next) => {
+	try {
+		// if user try to follow him/her-self
+		if (req.params.id === req.user.id) {
+			return res.status(400).json({
+				error: "You can't follow yourself",
+			});
+		}
+		// we need to get the follower and the one who will be followed to relate them together
+		let follower = await User.findById(req.user.id);
+		let followed = await User.findById(req.params.id);
+		if (follower.followings.includes(req.params.id)) {
+			return res.status(400).json({
+				error: "you already followed this user before",
+			});
+		}
+		await follower.updateOne({
+			$push: {
+				followings: req.params.id,
+			},
+		});
+		await followed.updateOne({
+			$push: {
+				followers: req.user.id,
+			},
+		});
+		return res.status(200).json({
+			data: "user is followed",
 		});
 	} catch (error) {
 		return res.status(500).json({
